7530ca72656751e66fe5a3932683681fdbdb2833
Eric L. Goldstein <eric.lucien.goldstein@gmail.com>
2022-02-20 18:36:12 -0500

    Remove React compat from WMR config

--- wmr.config.mjs
+++ wmr.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'wmr';
 
 // Full list of options: https://wmr.dev/docs/configuration
 export default defineConfig({
-	/* Your configuration here */
-	alias: {
-		react: 'preact/compat',
-		'react-dom': 'preact/compat'
-	}
+	out: './dist',
+	port: 8080,
+	publicPath: '/',
+	root: './public',
+	visualize: true,
+
+	// alias: {
+	// 	react: 'preact/compat',
+	// 	'react-dom': 'preact/compat'
+	// }
 });
