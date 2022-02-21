54f885747dd5e8b60ee00a9b9e054e501f2e9136
Eric L. Goldstein <eric.lucien.goldstein@gmail.com>
2022-02-21 04:47:03 -0500

    Fix builds

--- wmr.config.mjs
+++ wmr.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'wmr';
 
 // Full list of options: https://wmr.dev/docs/configuration
 export default defineConfig({
-	out: './dist',
-	port: 8080,
-	publicPath: '/',
-	root: './public',
-	visualize: true,
+	// out: './dist',
+	// port: 8080,
+	// publicPath: '/',
+	// root: './public',
+	// visualize: true,
 
 	// alias: {
 	// 	react: 'preact/compat',
